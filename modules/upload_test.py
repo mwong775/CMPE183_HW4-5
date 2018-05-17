0c42471ed664a1ab63e15bb1b3576cba6ce6cf4b
Luca de Alfaro <luca@dealfaro.org>
2018-05-17 13:46:51 -0700

    Something works

---
+++ modules/upload_test.py
@@ -0,0 +1,29 @@
+import requests
+import time
+
+API_URL = "http://upload-dot-luca-teaching.appspot.com/start/uploader/get_upload_url"
+
+IMG_FILE = "IMG_0542.JPG"
+
+headers = {}
+headers['Content-Type'] = 'image/jpeg'
+
+# First, gets an image upload URL.
+r = requests.get(API_URL)
+urls = r.json()
+put_url = urls['signed_url']
+get_url = urls['access_url']
+print "Urls received"
+print "  put_url:", put_url
+print "  get_url:", get_url
+
+# Uploads image.
+with open(IMG_FILE) as data:
+    req = requests.put(put_url, data=data, headers=headers)
+    print "Upload outcome:", req.status_code
+
+# Download image.
+time.sleep(1.0)
+req = requests.get(get_url)
+print "GET status:", req.status_code
+
