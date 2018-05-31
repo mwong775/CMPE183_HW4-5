c36d0ea501d7956bee8bab45e986430482f06526
Luca de Alfaro <luca@dealfaro.org>
2018-05-31 13:44:54 -0700

    cookies

--- controllers/api.py
+++ controllers/api.py
@@ -1,6 +1,60 @@
 # Here go your api methods.
 
-def check_login():
+import hashlib
+from gluon.utils import web2py_uuid
+
+SECRET_KEY = 'melanzana'
+
+def check():
     response.headers['Access-Control-Allow-Origin'] = '*'
-    is_logged_in = logged_in_user is not None
-    return response.json(dict(is_logged_in=is_logged_in))
+    youare = 'None' if logged_in_user is None else logged_in_user['username']
+    return response.json(dict(result=youare))
+
+def register():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    username = request.vars.username
+    # Checks new.
+    if not db(db.myuser.username == username).isempty():
+        return response.json(dict(result='duplicate'))
+    h = hashlib.sha256(SECRET_KEY)
+    h.update(request.vars.password)
+    token = web2py_uuid()
+    # Inserts the user
+    db.myuser.insert(
+        username = username,
+        password = h.hexdigest(),
+        token = token,
+    )
+    # Creates the cookie.
+    response.cookies['myapp'] = token
+    response.cookies['myapp']['path'] = '/' # Valid in all the app
+    response.cookies['myapp']['expires'] = 24 * 3600
+    return response.json(dict(result='created'))
+
+def login():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    username = request.vars.username
+    h = hashlib.sha256(SECRET_KEY)
+    h.update(request.vars.password)
+    r = db((db.myuser.username == username) &
+           (db.myuser.password == h.hexdigest())).select().first()
+    if r is None:
+        response.cookies['myapp'] = None
+        return response.json(dict(result='fail'))
+    # Creates a new token.
+    token = web2py_uuid()
+    r.update_record(token = token)
+    # Puts it in the cookie.
+    response.cookies['myapp'] = token
+    response.cookies['myapp']['path'] = '/' # Valid in all the app
+    response.cookies['myapp']['expires'] = 24 * 3600
+    response.cookies['myapp']['secure'] = False # For testing only.
+    return response.json(dict(result='logged in'))
+
+def logout():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.cookies['myapp'] = None
+    return response.json(dict(result='logged out'))
+
+
+
--- models/tables.py
+++ models/tables.py
@@ -9,20 +9,25 @@
 
 
 # Super minimal authentication.
-db.define_table('myusers',
+db.define_table('myuser',
                 Field('username'),
                 Field('password'),
                 Field('token'),
                 )
 
+# For testing ONLY, so we can use insecure tokens.
+session._secure = False
+
 # Gets the identity of the logged in user.
 logged_in_user = None
-cookie = request.cookies.get('myapp')
-if cookie is not None:
-    r = db(db.myuser.token == cookie).first()
-    if r is not None:
-        logged_in_user = dict(
-            id = r.id,
-            name = r.username
-        )
+if request.cookies.has_key('myapp'):
+    cookie = request.cookies['myapp'].value
+    logger.info("Cookie: %r" % cookie)
+    if cookie is not None:
+        r = db(db.myuser.token == cookie).first()
+        if r is not None:
+            logged_in_user = dict(
+                id = r.id,
+                name = r.username
+            )
 
