654be73bc50c919d429c30e12e94e102279cfe71
Luca de Alfaro <luca@dealfaro.org>
2018-05-31 12:52:33 -0700

    I can check logins

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,6 @@
 # Here go your api methods.
+
+def check_login():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    is_logged_in = logged_in_user is not None
+    return response.json(dict(is_logged_in=is_logged_in))
--- models/tables.py
+++ models/tables.py
@@ -8,7 +8,21 @@
 # Consult manual for more options, validators, etc.
 
 
+# Super minimal authentication.
+db.define_table('myusers',
+                Field('username'),
+                Field('password'),
+                Field('token'),
+                )
 
+# Gets the identity of the logged in user.
+logged_in_user = None
+cookie = request.cookies.get('myapp')
+if cookie is not None:
+    r = db(db.myuser.token == cookie).first()
+    if r is not None:
+        logged_in_user = dict(
+            id = r.id,
+            name = r.username
+        )
 
-# after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
