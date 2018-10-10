cd0e9b6da810811f4c120247c53e88c2e4ec7fb4
Luca de Alfaro <luca@dealfaro.org>
2018-10-10 15:43:13 -0700

    checkpoint

--- models/tables.py
+++ models/tables.py
@@ -7,7 +7,21 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
+# logger.info("The user record is: %r" % auth.user)
 
+import datetime
+
+def get_user_email():
+    return None if auth.user is None else auth.user.email
+
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+db.define_table('post',
+                Field('post_content', 'text'),
+                Field('post_time', 'datetime', update=get_current_time()),
+                Field('post_author', default=get_user_email()),
+                )
 
 
 # after defining tables, uncomment below to enable auditing
