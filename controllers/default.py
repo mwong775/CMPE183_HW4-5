81527c1a8b46d118f4be55113bcbc2da1ce038ad
Luca de Alfaro <luca@dealfaro.org>
2018-10-19 11:37:28 -0700

    Homework 2 ready

--- controllers/default.py
+++ controllers/default.py
@@ -11,232 +11,62 @@
 import datetime
 
 
-def index_with_many_queries():
-    rows = db(db.post.id > 0).select()
-    result = []
-    for r in rows:
-        starred = (False if auth.user is None else
-            db((db.star.post_id == r.id) & (db.star.user_id == auth.user.id)).select().first() is not None)
+def index():
+    result = [] # We will accummulate the result here.
+    for r in db(db.post.id > 0).select():
+        # This is a loop over all the posts.
+
+        # Each post can have replies; you need to read here the list of replies.
+        # As each reply has an author and a content, a list of dictionaries seems
+        # appropriate here.
+        reply_list=[]
+        replies = db(db.reply.post_id == r.id).select(orderby=db.reply.reply_time)
+        for reply in replies:
+            reply_list.append(dict(
+                reply_id = reply.id,
+                reply_author = reply.user_email,
+                reply_content = reply.reply_content,
+            ))
+
         result.append(dict(
             post_title=r.post_title,
             post_author=r.post_author,
             post_content=r.post_content,
-            starred=starred,
+            reply_list=reply_list,
             id=r.id,
         ))
+
     logger.info("Result: %r" % result)
     return dict(rows=result)
 
 
-def index():
-    result = [] # We will accummulate the result here.
-    if auth.user is None:
-        # We cannot give information on stars; we can simply give out a list of posts.
-        for r in db(db.post.id > 0).select():
-            result.append(dict(
-                post_title=r.post_title,
-                post_author=r.post_author,
-                post_content=r.post_content,
-                starred=False, # The button bar in any case should not be displayed if one is not logged in.
-                id=r.id,
-            ))
-    else:
-        # The user is logged in.  In this case, we can pull out of the db also the stars.
-        # To understand this version of the code, please see
-        # http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer#One-to-many-relation
-        # We are doing a "left outer join", because some posts may not be starred.
-        rows = db().select(db.post.ALL, db.star.ALL,
-                           left=db.star.on(
-                               (db.post.id == db.star.post_id) & # The star has to be for the post
-                               (db.star.user_id == auth.user.id) # And it must be from the logged in user.
-                           ))
+@auth.requires_login()
+def add_reply():
+    """COMPLETE (and remove line below or replace as appropriate)"""
+    return redirect(URL('default', 'index'))
 
-        for r in rows:
-            # Notice that here we avoid doing one query for each row.
-            # We already know whether things are starred or not.
-            result.append(dict(
-                post_title=r.post.post_title, # Notice how we have to say r.post.post_title rather than r.post_title.
-                post_author=r.post.post_author,
-                post_content=r.post.post_content,
-                starred=r.star.id is not None, # It does not matter here which field of star we check for existence.
-                id=r.post.id, # This is the id of the post, as before.
-            ))
-    logger.info("Result: %r" % result)
-    return dict(rows=result)
-
-
-def index_thumbs():
-    result = [] # We will accummulate the result here.
-    if auth.user is None:
-        # We cannot give information on stars; we can simply give out a list of posts.
-        for r in db(db.post.id > 0).select():
-            result.append(dict(
-                post_title=r.post_title,
-                post_author=r.post_author,
-                post_content=r.post_content,
-                starred=False, # The button bar in any case should not be displayed if one is not logged in.
-                id=r.id,
-            ))
-    else:
-        # The user is logged in.  In this case, we can pull out of the db also the stars.
-        # To understand this version of the code, please see
-        # http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer#One-to-many-relation
-        # We are doing a "left outer join", because some posts may not be starred.
-        rows = db().select(db.post.ALL, db.star.ALL,
-                           left=db.star.on(
-                               (db.post.id == db.star.post_id) & # The star has to be for the post
-                               (db.star.user_id == auth.user.id) # And it must be from the logged in user.
-                           ))
-        for r in rows:
-            # I need to get from the DB two additional things.
-            # First, I want the list of people who thumbed it up/down.
-            # Then, I also want to know if I (the logged in user) thumbed it up/down.
-            thumb_rows = db(db.thumb.post_id == r.post.id).select()
-            thumb_ups = []
-            thumb_downs = []
-            my_thumb = None
-            for thumb_row in thumb_rows:
-                # I am iterating on the thumbings for this particular post.
-                if thumb_row.thumb_state == 'u':
-                    thumb_ups.append(thumb_row.user_email)
-                elif thumb_row.thumb_state == 'd':
-                    thumb_downs.append(thumb_row.user_email)
-                if thumb_row.user_email == auth.user.email:
-                    my_thumb = thumb_row.thumb_state
-            # Notice that here we avoid doing one query for each row.
-            # We already know whether things are starred or not.
-            result.append(dict(
-                post_title=r.post.post_title, # Notice how we have to say r.post.post_title rather than r.post_title.
-                post_author=r.post.post_author,
-                post_content=r.post.post_content,
-                starred=r.star.id is not None, # It does not matter here which field of star we check for existence.
-                liked=thumb_ups,
-                disliked=thumb_downs,
-                my_thumb=my_thumb,
-                id=r.post.id, # This is the id of the post, as before.
-            ))
-    logger.info("Result: %r" % result)
-    return dict(rows=result)
-
-
+@auth.requires_login()
 @auth.requires_signature()
+def edit_reply():
+    """COMPLETE (and remove line below or replace as appropriate)"""
+    return redirect(URL('default', 'index'))
+
+
 @auth.requires_login()
-def thumb():
-    post_id = int(request.args[0])
-    # Let's use the "update_or_insert" database method, so our code will be more compact.
-    # See http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer#update_or_insert
-    db.thumb.update_or_insert(
-        (db.thumb.post_id == post_id) & (db.thumb.user_email == auth.user.email), # Query
-        post_id=post_id, # And fields.
-        user_email=auth.user.email,
-        thumb_state=request.args[1]
-    )
-    # That's it; the deed is done.
-    redirect(request.vars.next)
-
-
 @auth.requires_signature()
-@auth.requires_login()
-def thumb_unthumb():
-    """In this version of the code, clicking on an already clicked thumb deletes it."""
-    post_id = int(request.args[0])
-    thumb_record = db((db.thumb.post_id == int(request.args[0])) &
-                      (db.thumb.user_email == auth.user.email)).select().first()
-    new_state = request.args[1]
-    if thumb_record is None:
-        # We insert the new state.
-        db.thumb.insert(
-            user_email=auth.user.email,
-            post_id=post_id,
-            thumb_state=new_state,
-        )
-    else:
-        if thumb_record.thumb_state == new_state:
-            # Two clicks deletes the thumb. For delete_record, see
-            # See http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer#select
-            thumb_record.delete_record()
-        else:
-            # We update the record;
-            # see http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer#update_record
-            thumb_record.update_record(thumb_state=new_state)
-    # That's it; the deed is done.
-    redirect(request.vars.next)
-
-
-@auth.requires_signature()
-@auth.requires_login()
-def toggle_star():
-    star_record = db((db.star.post_id == int(request.args[0])) &
-        (db.star.user_id == auth.user_id)).select().first()
-    if star_record is not None:
-        # Removes star.
-        db(db.star.id == star_record.id).delete()
-    else:
-        # Adds the star.
-        db.star.insert(
-            user_id = auth.user.id,
-            post_id = int(request.args[0]))
-    redirect(request.vars.next) # Notice that now it's the request that tells us where to go next.
+def delete_reply():
+    """COMPLETE (and remove line below or replace as appropriate)"""
+    return redirect(URL('default', 'index'))
 
 
 @auth.requires_login()
-def add1():
-    """INSECURE -- form does not contain token -- do not use."""
-    """Very simple way to insert records."""
-    logger.info("Request method: %r", request.env.request_method)
-    if request.env.request_method == 'POST':
-        # This is a postback.  Insert the record.
-        logger.info("We are inserting: title: %r content: %r" % (
-            # I could also access the title via request.post_vars.post_title,
-            # but I don't care to differentiate betw post and get variables.
-            request.vars.post_title, request.vars.post_content
-        ))
-        db.post.insert(
-            post_title=request.vars.post_title,
-            post_content=request.vars.post_content
-        )
-        redirect(URL('default', 'index'))
-    else:
-        # This is a GET request.  Returns the form.
-        return dict()
-
-
-@auth.requires_login()
-def add2():
-    """More sophisticated way, in which we use web2py to come up with the form."""
-    form = SQLFORM.factory(
-        Field('post_title'),
-        Field('post_content', 'text'),
-    )
-    # We can process the form.  This will check that the request is a POST,
-    # and also perform validation, but in this case there is no validation.
-    if form.process().accepted:
-        # We insert the result, as in add1.
-        db.post.insert(
-            post_title=form.vars.post_title,
-            post_content=form.vars.post_content
-        )
-        # And we load default/index via redirect.
-        redirect(URL('default', 'index'))
-    # We ask web2py to lay out the form for us.
-    logger.info("My session is: %r" % session)
-    return dict(form=form)
-
-
-@auth.requires_login()
-def add3():
+def add():
     """More sophisticated way, in which we use web2py to come up with the form."""
     form = SQLFORM(db.post)
     # We can process the form.  This will check that the request is a POST,
     # and also perform validation, but in this case there is no validation.
     # THIS process() also inserts.
     if form.process().accepted:
-        # NOT NEEDED We insert the result, as in add1.
-        # db.post.insert(
-        #     post_title=form.vars.post_title,
-        #     post_content=form.vars.post_content
-        # )
-        # And we load default/index via redirect.
         redirect(URL('default', 'index'))
     # We ask web2py to lay out the form for us.
     logger.info("My session is: %r" % session)
@@ -247,18 +77,6 @@ def add3():
 @auth.requires_login()
 def edit():
     """Allows editing of a post.  URL form: /default/edit/<n> where n is the post id."""
-
-    # if len(request.args) == 0:
-    #     raise HTTP(500)
-    # try:
-    #     int_id = int(request.args[0])
-    # except:
-    #     raise HTTP(500)
-    # rows = db(db.post.id == int_id).select()
-    # for r in rows:
-    #     post = r
-    #     break
-    # # Now we have post.
 
     # For this controller only, we hide the author.
     db.post.post_author.readable = False
@@ -278,38 +96,6 @@ def edit():
         # The deed is done.
         redirect(URL('default', 'index'))
     return dict(form=form)
-
-
-@auth.requires_signature()
-@auth.requires_login()
-def delete():
-    post = db.post(request.args(0))
-    # We must validate everything we receive.
-    if post is None:
-        logging.info("Invalid edit call")
-        redirect(URL('default', 'index'))
-    # One can edit only one's own posts.
-    if post.post_author != auth.user.email:
-        logging.info("Attempt to edit some one else's post by: %r" % auth.user.email)
-        redirect(URL('default', 'index'))
-    db(db.post.id == post.id).delete()
-    redirect(URL('default', 'index'))
-
-
-# @auth.requires_login()
-# def edit1():
-#     post_id = request.vars.id # This matches the vars=dict(id=...) in index.html
-#     post = db.post(post_id)
-#     # We must validate everything we receive.
-#     if post is None:
-#         logging.info("Invalid edit call")
-#         redirect(URL('default', 'index'))
-#     form = SQLFORM.factory(
-#         Field('post_title'),
-#         Field('post_content', 'text'),
-#     )
-
-
 
 
 def user():
