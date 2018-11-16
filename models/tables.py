b5048c082274cc0f404e2e1300d0a83e19d95b05
Luca de Alfaro <luca@dealfaro.org>
2018-11-16 11:54:17 -0800

    Small improvements

--- models/tables.py
+++ models/tables.py
@@ -8,16 +8,16 @@
 # Consult manual for more options, validators, etc.
 
 
-DATE_FORMAT = '%Y-%m-%d %H:%M %Z'
+DATE_FORMAT = '%Y-%m-%d %H:%M'
 datetime_validator = IS_LOCALIZED_DATETIME(timezone=pytz.timezone(user_timezone), format=DATE_FORMAT)
-FULL_DATE_FORMAT = '%Y-%m-%d %H:%M:%S %Z'
+FULL_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 full_datetime_validator = IS_LOCALIZED_DATETIME(timezone=pytz.timezone(user_timezone), format=FULL_DATE_FORMAT)
 
 db.define_table('example_table',
                 Field('entry_name'),
                 Field('created_on', 'datetime', default=datetime.datetime.utcnow()))
 
-db.example_table.created_on.requires = datetime_validator
+db.example_table.created_on.requires = full_datetime_validator
 
 # nice_string = datetime_validator.formatter(utc_string)
 # utc_string, error_msg = datetime_validator(nice_string)
