437624c6aca91a2312e20a97dd32d9e906723276
Luca de Alfaro <luca@dealfaro.org>
2018-11-30 15:36:32 -0800

    added URL signature

--- controllers/api.py
+++ controllers/api.py
@@ -1,5 +1,7 @@
 # Here go your api methods.
 
+import url_signer
+
 @auth.requires_signature()
 def notify_upload():
     post_id = int(request.vars.post_id)
@@ -19,5 +21,7 @@ def get_image():
         image_url = None
     else:
         # Produce a signed URL for GET.
+        # Invents a random name for the image.
+        image_url = url_signer.gcs_url(r.image_name, verb='GET', expiration_secs=3600)
     return response.json(dict(image_url=image_url))
 
