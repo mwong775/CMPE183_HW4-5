8713a5bffb6a1ee41c3c2e8b5ccf954c5bd7f01c
Luca de Alfaro <luca@dealfaro.org>
2018-12-04 14:20:01 -0800

    Now also API attack surface

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,20 @@
 # Here go your api methods.
+
+@auth.requires_login()
+def insert_record():
+    db.mytable.insert(
+        title=request.vars.title,
+        paragraph=request.vars.paragraph,
+        quantity=int(request.vars.quantity)
+    )
+    return "ok"
+
+@auth.requires_signature()
+def insert_signed_record():
+    db.mytable.insert(
+        title=request.vars.title,
+        paragraph=request.vars.paragraph,
+        quantity=int(request.vars.quantity)
+    )
+    return "ok"
+
