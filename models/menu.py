ae09c151d7a52cf873664bd4585aae143b5bc61a
Luca de Alfaro <luca@dealfaro.org>
2018-12-04 13:53:42 -0800

    more work

--- models/menu.py
+++ models/menu.py
@@ -29,7 +29,7 @@ response.google_analytics_id = None
 # ----------------------------------------------------------------------------------------------------------------------
 
 response.menu = [
-    (T('Home'), False, URL('default', 'index'), [])
+    (T('Home site'), False, URL('default', 'index'), [])
 ]
 
 DEVELOPMENT_MENU = True
@@ -49,12 +49,10 @@ def _():
     # useful links to internal and external resources
     # ------------------------------------------------------------------------------------------------------------------
     response.menu += [
-        (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('Sample Menu 1'), False, '#', [
-            (T('Entry1'), False, URL('default', 'entry1')),
-            (T('Entry2'), False, URL('default', 'entry2')),
+        (T('Forms'), False, '#', [
+            (T('Strong form'), False, URL('forms', 'strong_form')),
+            (T('Weak form'), False, URL('forms', 'weak_form')),
         ]),
-        (T('Sample Menu 2'), False, URL('default', 'index'))
     ]
 
 
