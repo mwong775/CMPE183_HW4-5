f54ea3019c4b65185d672c7205d9fa5df5303f0d
Luca de Alfaro <luca@dealfaro.org>
2019-04-30 15:41:13 -0700

    db-examples-2019

---
+++ controllers/examples.py
@@ -0,0 +1,21 @@
+
+# For a POST form: 
+
+/start/default/edit
+[And then there is the post variables encoded in a "form" in HTTP]
+
+request.vars.x
+request.post_vars.x
+
+# If you do a GET form (don't):
+
+/start/default/edit?x=6
+
+# URL limit: 2048
+
+request.vars.x
+request.get_vars.x
+
+Luca"); DROP
+
+INSERT INTO Students(name) VALUES ("Luca\"); DROP");
